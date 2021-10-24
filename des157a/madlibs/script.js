6fe785c96854a43738b3038c7f3a4570304bb9bb
forgetsemicolon <pagarwal2610@gmail.com>
2021-10-23 18:57:10 -0700

    letters added to overlay

--- des157a/madlibs/script.js
+++ des157a/madlibs/script.js
@@ -23,16 +23,85 @@
         }
     })
 
+    function getImgURL(url) {
+        return ("./images/" + url);
+    }
+
     function makeMadlib(wordsArray) {
-        var myText = `Here are the words: ${wordsArray[0]}, ${wordsArray[1]}, ${wordsArray[2]}, ${wordsArray[3]}, ${wordsArray[4]}`;
-        var myText = `I have your kid. ${wordsArray[4]} was kidnapped from the ${wordsArray[0]} yesterday night while ${wordsArray[1]} with their pet ${wordsArray[2]}. You will only get your child back if you sponsor my trip to ${wordsArray[3]}`;
-        madlib.innerHTML = myText;
+        // const myText = `Here are the words: ${wordsArray[0]}, ${wordsArray[1]}, ${wordsArray[2]}, ${wordsArray[3]}, ${wordsArray[4]}`;
+
+        const myText = `I have your kid. ${wordsArray[4]} was kidnapped from the ${wordsArray[0]} yesterday night while ${wordsArray[1]} with their pet ${wordsArray[2]}. You will only get your child back if you sponsor my trip to ${wordsArray[3]}`;
+
+        const myText1 = "I have your kid.";
+
+        madlib.innerHTML = myText1;
+
+        for (let eachLetter of wordsArray[4]) {
+            console.log("helloooo" + eachLetter);
+            var img = document.createElement("img");
+            img.src = getImgURL(`DES157-alpha-${eachLetter}.png`);
+            console.log(img.src);
+            madlib.appendChild(img);
+        };
+
+        const myText2 = " was kidnapped from the "
+
+        madlib.innerHTML += myText2;
+
+        for (let eachLetter of wordsArray[0]) {
+            console.log("helloooo" + eachLetter);
+            var img = document.createElement("img");
+            img.src = getImgURL(`DES157-alpha-${eachLetter}.png`);
+            console.log(img.src);
+            madlib.appendChild(img);
+        };
+
+        const myText3 = " yesterday night while "
+
+        madlib.innerHTML += myText3;
+
+        for (let eachLetter of wordsArray[1]) {
+            console.log("helloooo" + eachLetter);
+            var img = document.createElement("img");
+            img.src = getImgURL(`DES157-alpha-${eachLetter}.png`);
+            console.log(img.src);
+            madlib.appendChild(img);
+        };
+
+        const myText4 = " with their pet "
+
+        madlib.innerHTML += myText4;
+
+        for (let eachLetter of wordsArray[2]) {
+            console.log("helloooo" + eachLetter);
+            var img = document.createElement("img");
+            img.src = getImgURL(`DES157-alpha-${eachLetter}.png`);
+            console.log(img.src);
+            madlib.appendChild(img);
+        };
+
+        const myText5 = ". You will only get your child back if you sponsor my trip to "
+
+        madlib.innerHTML += myText5;
+
+        for (let eachLetter of wordsArray[3]) {
+            console.log("helloooo" + eachLetter);
+            var img = document.createElement("img");
+            img.src = getImgURL(`DES157-alpha-${eachLetter}.png`);
+            console.log(img.src);
+            madlib.appendChild(img);
+        };
+
+        const myText6 = "."
+
+        madlib.innerHTML += myText6;
+
     }
 
     function processData(formData) {
         let emptyFields = 0;
         let words = [];
-        for (var eachWord of formData) {
+        for (let eachWord of formData) {
             if (eachWord.value) {
                 words.push(eachWord.value);
                 eachWord.value = "";
